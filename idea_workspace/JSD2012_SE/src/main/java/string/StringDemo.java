b6451c34199a3c7bcc4ce4a68148cc3ff1548b51
Izaro <wudihh666@gmail.com>
2021-01-22 12:00:49 +0800

    4

--- idea_workspace/JSD2012_SE/src/main/java/string/StringDemo.java
+++ idea_workspace/JSD2012_SE/src/main/java/string/StringDemo.java
@@ -6,7 +6,7 @@ package string;
 public class StringDemo {
     public static  void main(String [] args){
         System.out.println("HAHA");
-        Syetem.out println("HelloWorld!!");
+        System.out.println("HelloWorld!!");
     }
 
 }
